e46bcecb8eba41c89ac9fc50ceabac1d76b95c1d
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2023-10-05 03:09:18 -0700

    First Commit
    Uploading files from local repo into Github

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about-section">
+      <div className="about-section-image">
+        <img src="./img/about-me.png" alt="About me" />
+      </div>
+
+      <div className="hero-section-content-box about-section-box">
+        <div className="hero-section-content">
+          <p className="section-title">About</p>
+          <h1 className="skills-section-heading">About Me</h1>
+          <p className="hero-section-description">
+            Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+            reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+            ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+            Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+            voluptate dolor minim nulla est proident. Nostrud officia pariatur
+            ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+            reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+            officia voluptate. Culpa proident adipisicing id nulla nisi laboris
+            ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
+            ex non excepteur duis sunt velit enim. Voluptate laboris sint
+            cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import HeroSection from "../HeroSection";
+import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
+import MyPortfolio from "../MyPortfolio";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <MySkills />
+      <AboutMe />
+      <MyPortfolio />
+    </>
+  );
+}
