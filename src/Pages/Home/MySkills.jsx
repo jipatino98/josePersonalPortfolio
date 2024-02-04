c1281cc4ef2f73349443f7df91530dea37d1a53a
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2024-02-04 01:40:43 -0800

    Removed section title in skills section

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -18,7 +18,7 @@ export default function MySkills() {
   return (
     <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
-        <p className="section--title">My Skills</p>
+        {/* <p className="section--title">My Skills</p> */}
         <h2 className="skills--section--heading">Experience</h2>
       </div>
 
